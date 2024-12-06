3ee7dcf19e65dd8d67e9b44ca173baf9e5cfc4c8
Csilla Toth <csillaout@gmail.com>
2024-12-06 13:39:48 +0000

    web

--- advanced_python/web_scraping/exercise_2/amazon.py
+++ advanced_python/web_scraping/exercise_2/amazon.py
@@ -5,7 +5,7 @@
 
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+
 
 
 url = "https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue"
@@ -17,7 +17,8 @@ table = soup.find_all('table')[0]
 world_titles = table.find_all('th')
 world_table_titles = [title.text.strip() for title in world_titles]
 print(world_table_titles) 
-import pandas as pd
-print(pd.__version__)
-df = pd.DataFrame(columns = world_table_titles)
-df
+column_data = table.find_all('tr')
+for row in column_data:
+    row_data = row.find_all('td')
+    individual_row_data=[data.text.strip() for data in row_data]
+    print(individual_row_data)
